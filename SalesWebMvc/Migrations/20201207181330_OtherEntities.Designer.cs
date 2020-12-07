57b2eea794240f4f8c0caa24b51dcf8555375395
rafael <rafaellucasdacosta42@gmail.com>
2020-12-07 15:39:48 -0300

    Seeding service

--- SalesWebMvc/Migrations/20201207181330_OtherEntities.Designer.cs
+++ SalesWebMvc/Migrations/20201207181330_OtherEntities.Designer.cs
@@ -3,8 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using SalesWebMvc.Data;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Migrations
 {
